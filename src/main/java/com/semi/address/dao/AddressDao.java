440315a50dee34771b373ec9233351a341fc9aac
junhee <joooonh12@gmail.com>
2022-12-19 10:32:15 +0900

    연락처 추가, 연락처 상세보기

--- src/main/java/com/semi/address/dao/AddressDao.java
+++ src/main/java/com/semi/address/dao/AddressDao.java
@@ -1,10 +1,23 @@
 package com.semi.address.dao;
 
+import java.util.List;
+
 import com.semi.address.vo.Address;
 import com.semi.util.SqlMapper;
 
 public class AddressDao {
 
+	private static AddressDao instance = new AddressDao();
+	private AddressDao() {}
+	public static AddressDao getInstance() {
+		return instance;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Address> getAddressesByBookNo(int bookNo) {
+		return (List<Address>) SqlMapper.selectList("addresses.getAddressesByBookNo", bookNo);
+	}
+	
 	public void insertAddress(Address address) {
 		SqlMapper.insert("addresses.insertAddress", address);
 	}
--- src/main/java/com/semi/address/vo/Book.java
+++ src/main/java/com/semi/address/vo/Book.java
@@ -19,6 +19,8 @@ public class Book {
 	private Date deletedDate;
 	private Integer employeeNo;
 	
+	public Book() {}
+	
 	public int getBookNo() {
 		return bookNo;
 	}
